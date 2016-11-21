049ed7d7debec09b08d896a9c8017736671ebea4
Robin <robin@ufl.edu>
2016-11-21 14:40:16 -0500

    Created a new file for theanalysis named houseelf-analysis.R
    DNA samples and ear measurements from a small group of house-elves

---
+++ houseelf-analysis.R
@@ -0,0 +1,13 @@
+###Assignment 09
+
+##1. -- Set Up Git --
+
+##2. -- First Commit --
+
+##This is a follow up to Set Up Git. Create a new file for your analysis named houseelf-analysis.R 
+#and add a comment at the top describing what the analysis is intended to do.
+
+##3. -- Importing Data --
+
+house_elves_ears =read.csv("data/houseelf-earlength-dna-data.csv", header=T)
+head(house_elves_ears)
